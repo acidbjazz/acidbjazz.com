101aa2057ff64381585c2f9a271767602fcbf1e9
acidbjazz <acidbjazz@gmail.com>
2021-09-29 06:20:26 -0500

    intersection observer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 import { GraphQLClient } from "graphql-request";
 import useSWR from "swr";
-import Lottie from "react-lottie";
-import animationData from "../img/cat.json";
+import Lottie from "lottie-react";
+// import { useLottie, useLottieInteractivity } from "lottie-react";
+import animationData from "../img/star.json";
 import Icon from "./Icon";
+import { useEffect, useRef, useState } from "react";
 
 const endpoint = `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CSI}`;
 const graphQLClient = new GraphQLClient(endpoint, {
@@ -26,10 +28,45 @@ const Header = () => {
     `,
     fetcher
   );
+  const lottieRef = useRef();
+  // const divRef = useRef();
+  // console.log("divRef:", divRef.current);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const doSomething = (entries) => {
+    const [entry] = entries;
+    console.log("entry:", entry);
+    setIsVisible(entry.isIntersecting);
+    if (entry.isIntersecting) {
+      lottieRef.current.play();
+    } else {
+      lottieRef.current.pause();
+    }
+  };
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(doSomething, options);
+    if (lottieRef.current.animationItem) observer.observe(lottieRef.current?.animationItem?.wrapper);
+  }, [lottieRef, options]);
+
   return (
     <header>
       <div className="lottie-cat">
-        <Lottie options={{ animationData, loop: true, autoplay: true, rendererSettings: { className: "cat" } }} />
+        <Lottie
+          // onComplete={() => lottieRef.current.goToAndStop(1)}
+          animationData={animationData}
+          loop={true}
+          autoplay={true}
+          lottieRef={lottieRef}
+          className="cat"
+          style={isVisible ? { border: "1px solid red" } : { border: "1px solid blue" }}
+        />
       </div>
       <h1>{data?.dataCollection.items[0].name}</h1>
       <p>{data?.dataCollection.items[0].jobTitle}</p>
@@ -47,6 +84,11 @@ const Header = () => {
         <a href="https://open.spotify.com/user/12124188210" target="_blank" rel="noreferrer">
           <Icon name={"spotify"} />
         </a>
+      </div>
+      <div className="control">
+        {/* <button onClick={() => setIsStopped(true)}>stop</button> */}
+        <button onClick={() => lottieRef.current.goToAndPlay(1)}>goToAndPlay(1)</button>
+        {/* <button onClick={() => setIsPaused(!isPaused)}>pause</button> */}
       </div>
     </header>
   );
